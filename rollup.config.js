974640d873ca4f3a0689ec456e4eccb8fbb04e6a
Paul K <paul@paulkr.com>
2026-02-04 10:49:57 -0500

    fix: npm vulnerabilities (#33)

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,24 @@
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import copy from 'rollup-plugin-copy';
-import pkg from './package.json';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import json from '@rollup/plugin-json';
+import { createRequire } from 'module';
 
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
 
 export default {
   input: 'src/index.ts',
   external: [],
   output: [
-    { file: pkg.main, format: 'cjs' },
+    { file: pkg.main, format: 'cjs', exports: 'auto' },
     { file: pkg.module, format: 'es' },
   ],
   plugins: [
     typescript({
-      rollupCommonJSResolveHack: false,
       clean: true,
     }),
     json(),
@@ -29,7 +30,6 @@ export default {
       preferBuiltins: false,
       extensions: ['.mjs', '.js', '.json', '.node', '.jsx', '.ts', '.tsx'],
     }),
-    // nodePolyfills(),
     babel({
       exclude: 'node_modules/**',
       babelHelpers: 'bundled',
